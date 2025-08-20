7eea40d56a5ef44f89ff52eb5c3bfb865b42ce20
jian <jiajiantang123@gmail.com>
2025-08-20 10:27:10 +0800

    left howitworks

---
+++ WhyOura2/ScienceAndResearch.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('learnMore').addEventListener('click', () => {
+    window.location.href = '../HowItWorks/HowItWorks.html';
+  })
+});
